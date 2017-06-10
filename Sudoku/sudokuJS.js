303a82b5f2acae545e6da8d9bdbbd9af72dab1a6
Goncharov Cyril <Krio05@yandex.ua>
2017-06-10 22:03:19 +0300

    Добавил перемешивание чисел на доске

--- Sudoku/sudokuJS.js
+++ Sudoku/sudokuJS.js
@@ -11,6 +11,36 @@ for (var i = 0; i < 4; i++)
         matrix[i * 4 + j] = (i * 2 + Math.floor(i / 2) + j) % 4 + 1;
 
 
+
+
+
+
+
+
+for (var i = 0; i < 100; i++) {
+    var n1 = Math.ceil(Math.random() * 4);
+    var n2;
+    do {
+        n2 = Math.ceil(Math.random() * 4);
+    }
+    while (n1 == n2);
+
+    for (var row = 0; row < 4; row++) {
+        for (var col = 0; col < 4; col++) {
+            if (matrix[row * 4 + col] == n1)
+                matrix[row * 4 + col] = n2;
+            else if (matrix[row * 4 + col] == n2)
+                matrix[row * 4 + col] = n1;
+        }
+    }
+}
+
+
+
+
+
+
+
 var table = document.createElement("table");
 table.border = 1;
 var trs = new Array(4);
