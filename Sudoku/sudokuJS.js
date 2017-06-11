78844b1d77032a9b9d71cab38ea09762e02b6466
Goncharov Cyril <Krio05@yandex.ua>
2017-06-11 08:39:05 +0300

    Добавил докблоки ко всем методам и глобальным полям

--- Sudoku/sudokuJS.js
+++ Sudoku/sudokuJS.js
@@ -1,7 +1,23 @@
+/**
+* Элемент html страницы div
+*
+* Используется для хранения 
+* динамически созданных 
+* элементов
+*
+* @var div articleDiv
+*/
 var articleDiv = document.querySelector("div.base");
 
 
-
+/**
+* Матрица доски судоку
+*
+* Используется для хранения 
+* значений доски судоку 4*4
+*
+* @var int[] matrix
+*/
 var matrix = new Array(16);
 
 
@@ -50,6 +66,13 @@ for (var c = 0; c < 100; c++) {
 for (var i = 0; i < 2; i++) {
     for (var j = 0; j < 2; j++) {
         for (var k = 0; k < 2; k++) {
+            /**
+            * Номер ячейки доски
+            *
+            * Случайная ячейка для маскировки
+            *
+            * @var int c
+            */
             var c;
             do {
                 c = Math.floor(Math.random() * 4);
@@ -65,15 +88,54 @@ for (var i = 0; i < 2; i++) {
 
 
 
-
+/**
+* Таблица доски
+*
+* Используется для вывода  
+* доски на страницу
+*
+* @var table table
+*/
 var table = document.createElement("table");
 table.border = 1;
+/**
+* Строки таблицы
+*
+* Массив строк, используется для  
+*  хранения строк таблицы
+*
+* @var tr[] trs
+*/
 var trs = new Array(4);
+/**
+* Столбцы таблицы
+*
+* Массив столбцов, используется для  
+*  хранения столбцов таблицы
+*
+* @var tr[] trs
+*/
 var tds = new Array(16);
 
-
-
+/**
+* Изменяемые ячейки
+*
+* Массив полей ввода, используется 
+* для хранения полей ввода пустых  
+* ячеек таблицы
+*
+* @var input[] inputs
+*/
 var inputs = new Array(8);
+/**
+* Номера пустых ячеек
+*
+* Массив индексов, используется 
+* для хранения номеров пустых  
+* ячеек таблицы
+*
+* @var int[] inpinds
+*/
 var inpinds = new Array(8);
 
 for (var i = 0; i < 8; i++) {
@@ -81,12 +143,29 @@ for (var i = 0; i < 8; i++) {
     inputs[i].type = "text";
 }
 
+
+/**
+* Событие ввода для поля 1
+*
+* Переназначить событие ввода
+* для поля ввода 1 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[0].oninput = function () {
     var text = "";
     text = inputs[0].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[0].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 1
+*
+* Переназначить событие изменения
+* для поля ввода 1 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[0].onchange = function () {
     var line = Math.floor(inpinds[0] / 4);
     var column = inpinds[0] % 4;
@@ -97,12 +176,28 @@ inputs[0].onchange = function () {
     }
     matrix[inpinds[0]] = inputs[0].value
 }
+/**
+* Событие ввода для поля 2
+*
+* Переназначить событие ввода
+* для поля ввода 2 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[1].oninput = function () {
     var text = "";
     text = inputs[1].value;
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[1].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 2
+*
+* Переназначить событие изменения
+* для поля ввода 2 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[1].onchange = function () {
     var line = Math.floor(inpinds[1] / 4);
     var column = inpinds[1] % 4;
@@ -113,12 +208,28 @@ inputs[1].onchange = function () {
     }
     matrix[inpinds[1]] = inputs[1].value
 }
+/**
+* Событие ввода для поля 3
+*
+* Переназначить событие ввода
+* для поля ввода 3 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[2].oninput = function () {
     var text = "";
     text = inputs[2].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[2].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 3
+*
+* Переназначить событие изменения
+* для поля ввода 3 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[2].onchange = function () {
     var line = Math.floor(inpinds[2] / 4);
     var column = inpinds[2] % 4;
@@ -129,12 +240,28 @@ inputs[2].onchange = function () {
     }
     matrix[inpinds[2]] = inputs[2].value
 }
+/**
+* Событие ввода для поля 4
+*
+* Переназначить событие ввода
+* для поля ввода 4 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[3].oninput = function () {
     var text = "";
     text = inputs[3].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[3].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 4
+*
+* Переназначить событие изменения
+* для поля ввода 4 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[3].onchange = function () {
     var line = Math.floor(inpinds[3] / 4);
     var column = inpinds[3] % 4;
@@ -145,12 +272,28 @@ inputs[3].onchange = function () {
     }
     matrix[inpinds[3]] = inputs[3].value
 }
+/**
+* Событие ввода для поля 5
+*
+* Переназначить событие ввода
+* для поля ввода 5 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[4].oninput = function () {
     var text = "";
     text = inputs[4].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[4].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 5
+*
+* Переназначить событие изменения
+* для поля ввода 5 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[4].onchange = function () {
     var line = Math.floor(inpinds[4] / 4);
     var column = inpinds[4] % 4;
@@ -161,12 +304,28 @@ inputs[4].onchange = function () {
     }
     matrix[inpinds[4]] = inputs[4].value
 }
+/**
+* Событие ввода для поля 6
+*
+* Переназначить событие ввода
+* для поля ввода 6 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[5].oninput = function () {
     var text = "";
     text = inputs[5].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[5].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 6
+*
+* Переназначить событие изменения
+* для поля ввода 6 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[5].onchange = function () {
     var line = Math.floor(inpinds[5] / 4);
     var column = inpinds[5] % 4;
@@ -177,12 +336,28 @@ inputs[5].onchange = function () {
     }
     matrix[inpinds[5]] = inputs[5].value
 }
+/**
+* Событие ввода для поля 7
+*
+* Переназначить событие ввода
+* для поля ввода 7 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[6].oninput = function () {
     var text = "";
     text = inputs[6].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[6].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 7
+*
+* Переназначить событие изменения
+* для поля ввода 7 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[6].onchange = function () {
     var line = Math.floor(inpinds[6] / 4);
     var column = inpinds[6] % 4;
@@ -193,12 +368,28 @@ inputs[6].onchange = function () {
     }
     matrix[inpinds[6]] = inputs[6].value
 }
+/**
+* Событие ввода для поля 8
+*
+* Переназначить событие ввода
+* для поля ввода 8 пустой
+* ячейки. Служит для контроля ввода
+* цифр.
+*/
 inputs[7].oninput = function () {
     var text = "";
     text = inputs[7].value
     if (text[text.length - 1] != 1 && text[text.length - 1] != 2 && text[text.length - 1] != 3 && text[text.length - 1] != 4)
         inputs[7].value = text.substring(0, text.length - 1);
 }
+/**
+* Событие изменения для поля 8
+*
+* Переназначить событие изменения
+* для поля ввода 8 пустой
+* ячейки. Служит для проверки
+* ячейки на совпадения в строке/столбце.
+*/
 inputs[7].onchange = function () {
     var line = Math.floor(inpinds[7] / 4);
     var column = inpinds[7] % 4;
